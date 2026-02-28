7c1497444aaacb91d225746e356f8b288f750ee7
Marco Antonio - The Way of the Artisan <markitos.es.info@gmail.com>
2026-02-28 18:01:30 +0100

    fix: Update coverage report format and remove obsolete test script

--- bin/run-tests.sh
+++
@@ -1,5 +0,0 @@
-go test ./...                    # todos
-go test ./... -v                 # verbose
-go test ./... -v -run TestBump   # solo los de bump
-go test ./... -cover             # con cobertura
-go test ./... -coverprofile=coverage.out && go tool cover -html=coverage.out
